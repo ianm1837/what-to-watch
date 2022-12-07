54063c4458b11f53db846defa0704e3007bc032e
ianm1837 <ian@iwm.dev>
2022-12-06 19:34:23 -0800

    Update script.js
    added comments to outline code structure

--- js/script.js
+++ js/script.js
@@ -13,3 +13,34 @@ fetch('https://imdb-top-100-movies.p.rapidapi.com/premiummovies', options)
 	.catch(err => console.error(err));
 
     console.log()
+
+
+
+//on click - function to get movies based on parameter, load showcase page, update dom
+
+
+
+	//get genre and rating element values
+
+
+	//set variables for api call
+
+
+	//make api call
+
+
+	//inside .then after API call load showcase
+
+
+		//take returned data and pick random movie on page
+
+
+		//update showcase elements with returned API data
+
+
+
+
+//get button element by id
+
+
+//even listener for button to call getTheMovieDatabaseApi
