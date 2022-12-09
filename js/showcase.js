219460d6968b4b824fe82b9ff7acae702bd5e5dc
rayjohn666 <115908599+rayjohn666@users.noreply.github.com>
2022-12-08 20:58:28 -0800

    Update showcase.js
    fixed imdb link

--- js/showcase.js
+++ js/showcase.js
@@ -7,13 +7,13 @@ var movieLink = document.getElementById('movie-link')
 var storedPoster = localStorage.getItem('poster-url')
 var storedTitle = localStorage.getItem('movie-title')
 var storedDescription = localStorage.getItem('movie-overview')
-var storedRating = localStorage.getItem('ratin')
+var storedRating = localStorage.getItem('rating')
 var storedLink = localStorage.getItem('imdb-id')
 
 // storedRating.toString()
 
-moviePoster.innerHTML = `<img src=https://image.tmdb.org/t/p/original/${storedPoster}>`
+moviePoster.innerHTML = `<img src="https://image.tmdb.org/t/p/original/${storedPoster}">`
 movieTitle.innerHTML = `<h3 class="is-size-2">${storedTitle}</h3>`
 movieDescription.innerHTML = `<p>${storedDescription}</p>`
 movieRating.innerHTML = `${storedRating}`
-storedLink.innerHTML = `<a href=https://www.imdb.com/title/${storedLink}>View on IMDB</a>`
+movieLink.innerHTML = `<a href="https://www.imdb.com/title/${storedLink}">View on IMDB</a>`
